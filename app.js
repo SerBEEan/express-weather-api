81d7c61f7ca7e7d0a99f78d69bd510242c933fcf
SerBEEan <sergey.atwork7@gmail.com>
2022-06-04 23:54:12 +0300

    Добавил метрики через Prometheus-Grafana

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const promBundle = require("express-prom-bundle");
 const weatherRouter = require('./weatherRouter');
+
+const metricsMiddleware = promBundle({ includeMethod: true, includePath: true });
 
 const app = express();
 const { PORT } = process.env;
 
+app.use(metricsMiddleware);
 app.use(express.json());
 app.use('/v1', weatherRouter);
 
